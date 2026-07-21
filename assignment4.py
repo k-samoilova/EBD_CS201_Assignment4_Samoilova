82d672755d8f9d35e05dca2adcc59ef9c20d6dee
k-samoilova <kateryna.samoilova.25@kse.org.ua>
2026-07-21 13:40:03 +0300

    changing "N/A" values to 0; changing all values from "quantity" "revenue" and tariffs to integers

--- assignment4.py
+++ assignment4.py
@@ -1,10 +1,24 @@
 import csv
+import json
 
 with open("global_sales.csv", "r", encoding="utf-8") as file:
-    list_of_dictionaries = csv.reader(file)
+    list_of_dictionaries = list(csv.DictReader(file))
 
-import json
 
 with open("regional_tariffs.json", "r", encoding="utf-8") as file:
     dictionary = json.load(file)
+
+for x in list_of_dictionaries:
+    if x["quantity"] == "N/A":
+        x["quantity"] = 0
+    if x["revenue"] == "N/A":
+        x["revenue"] = 0
+    x["quantity"] = int(x["quantity"])
+    x["revenue"] = int(x["revenue"])
+
+for a in dictionary:
+    if dictionary[a] == "N/A":
+        dictionary[a] = 0
+    else:
+        dictionary[a] = int(dictionary[a])
 
